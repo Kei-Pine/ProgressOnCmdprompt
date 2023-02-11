50b4aadd5ea9c49039c9355ae021a421fe772f08
you dont know me <80095166+Kei-Pine@users.noreply.github.com>
2023-02-11 13:10:32 +0000

    minor change

--- prospin.c
+++ prospin.c
@@ -4,8 +4,8 @@
 int main() {
     char spin[4] = {'|', '/', '-', '\\'};
     int i;
-    for (i = 0; i < 100; i++) {
-        printf("\r Loading %c %d", spin[i % 4],i);
+    for (i = 0; i <=100; i++) {
+        printf("\r Loading %c %d％", spin[i % 4],i);
         fflush(stdout);
         usleep(100000);
     }
